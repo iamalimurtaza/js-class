10684a3f340481b15732d1615a8dcd0a5f888131
Ali Murtaza <ali@badoatech.com>
2020-12-31 13:36:35 +0500

    JavaScript Class One

---
+++ classOne.js
@@ -0,0 +1,44 @@
+// assign =
+// equal ==
+// strict equal / identical ===
+// ommitting means avoiding
+// string means text
+
+// Variables ===
+// var keyword
+// var firstName = 'Intzar';
+// var lastName = 'Ali';
+// var age = 32;
+// var firstName = 'Ali';
+// var lastName = 'Hassan';
+// console.log(firstName, lastName, age);
+// console.log(firstName + ' ' + lastName);
+// var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// concatination
+// var shortBio = 'My first name is: ' + firstName + ', my last name is: ' + lastName + ', my age is: ' + age + '.';
+
+// Template litrals
+// var bio = `My first name is: ${firstName}, my last name is: ${lastName}, and my age is: ${age}`;
+// console.log(bio);
+
+// let keyword
+// let firstName = 'Ali';
+// let lastName = 'Hassan';
+// let age = 32;
+
+// firstName = 'Ali';
+// lastName = 'Hassan';
+// age = 24;
+// console.log(`My first name is: ${firstName}, my last name is: ${lastName}, and my age is: ${age}`);
+
+// const keyword
+// const firstName = 'Ali';
+// const lastName = 'Hassan';
+// const age = 24;
+
+// const firstName = 'Ali';
+// const lastName = 'Hassan';
+// const age = 24;
+// console.log(`My first name is: ${firstName}, my last name is: ${lastName}, and my age is: ${age}`);
