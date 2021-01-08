e451a548f52eddbafacda5cb2a3f317298f3977c
Ali Murtaza <ali@badoatech.com>
2021-01-08 16:24:49 +0500

    Class Two and Class Three added

---
+++ classThree.js
@@ -0,0 +1,91 @@
+// ==== scope ====
+
+// let firstName = 'Ali';
+// console.log('----Coming from global scope initially-------');
+// console.log(firstName);
+
+// // let x = 'Intzar';
+
+// if (true) {
+// 	firstName = 'Asad ';
+// 	let lastName = 'Minhas';
+// 	console.log('----Coming from local scoope-------');
+// 	console.log(firstName);
+// 	console.log('----Coming from local scoope-------');
+// 	console.log(lastName);
+// }
+
+// console.log('----Coming from global scope-------');
+// console.log(firstName);
+// console.log('----Coming from global scope-------');
+// console.log(lastName);
+
+// === array methods ===
+let badoaTechTeam = ['Ali', 'Asad', 'Ali', 'Intzar'];
+// badoaTechTeam[4] = 'Iqbal';
+// console.log(badoaTechTeam);
+// console.log(badoaTechTeam);
+// console.log(badoaTechTeam);
+// console.log('---- before popping ----');
+// console.log(badoaTechTeam);
+// push method
+// console.log(badoaTechTeam);
+// badoaTechTeam.push('Iftikhar');
+// badoaTechTeam.push('Iqbal');
+// badoaTechTeam.push('Mansoor');
+
+// console.log('---- after pushing ----');
+// console.log(badoaTechTeam);
+
+// pop method
+// badoaTechTeam.pop();
+// badoaTechTeam.pop();
+// badoaTechTeam.pop();
+// console.log('---- after popping ----');
+// console.log(badoaTechTeam);
+
+// let poppedFromBadoateTechTeam = badoaTechTeam.pop();
+// console.log(poppedFromBadoateTechTeam);
+// console.log(badoaTechTeam);
+// badoaTechTeam.push(poppedFromBadoateTechTeam);
+// console.log(badoaTechTeam);
+
+// shift
+// console.log(badoaTechTeam);
+// console.log(badoaTechTeam.shift());
+// console.log(badoaTechTeam);
+// console.log(badoaTechTeam.shift());
+// console.log(badoaTechTeam);
+// console.log(badoaTechTeam.shift());
+// console.log(badoaTechTeam);
+
+// Unshift
+// console.log(badoaTechTeam.unshift('Iqbal'));
+// console.log(badoaTechTeam);
+
+// Includes
+// console.log(badoaTechTeam.includes('Iqbal'));
+
+// indexOf
+// console.log(badoaTechTeam);
+// console.log(badoaTechTeam.indexOf('Ali'));
+// console.log(badoaTechTeam[3]);
+
+// aray length
+// console.log(badoaTechTeam.length);
+
+// String methods
+// let fullName = 'Ali Murtaza';
+// let x = fullName.toLowerCase();
+// let x = fullName.toLowerCase();
+// console.log(x);
+
+// Math
+// let x = 2.5;
+// console.log(Math.PI);
+// console.log(Math.pow(2, 5));
+// console.log(Math.sqrt(49));
+// console.log(Math.floor(x));
+// console.log(Math.ceil(x));
+// console.log(Math.round(x));
+// console.log(Math.random());
